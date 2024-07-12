5aa91f05a819bd406d85149fa86e16dd14d90a49
samidev016 <adelim0555@gmail.com>
2024-07-12 21:23:29 +0200

    setup everything

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-     
-      home: const MyHomePage(),
+      title: 'Flutter Hive Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.black,
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle1: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 21,
+          ),
+          headline3: TextStyle(
+            color: Color.fromARGB(255, 234, 234, 234),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+          headline4: TextStyle(
+            color: Colors.grey,
+            fontSize: 17,
+          ),
+          headline5: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+          headline6: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+      home: const Aa(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class Aa extends StatelessWidget {
+  const Aa({super.key});
 
   @override
   Widget build(BuildContext context) {
