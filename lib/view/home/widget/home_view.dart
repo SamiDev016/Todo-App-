7bdbc86100046b7942807495cf4af1b6dbafc8f0
samidev016 <adelim0555@gmail.com>
2024-07-13 21:00:11 +0200

    home_view_dart

--- lib/view/home/widget/home_view.dart
+++ lib/view/home/widget/home_view.dart
@@ -14,22 +14,16 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
       backgroundColor: Colors.white,
-
-
       floatingActionButton: Fab(),
-
       body: SizedBox(
         height: double.infinity,
         width: double.infinity,
         child: Column(
           children: [
-
-
             //Costum App BAR
             Container(
               margin: const EdgeInsets.only(top: 60),
@@ -44,26 +38,26 @@ class _HomeViewState extends State<HomeView> {
                     child: CircularProgressIndicator(
                       value: 0.3,
                       backgroundColor: Colors.grey,
-                      valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),
+                      valueColor:
+                          AlwaysStoppedAnimation(AppColors.primaryColor),
                     ),
+                  ),
+                  25.w,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        AppStr.mainTitle,
+                        style: textTheme.displayLarge,
+                      ),
+                      3.h,
+                      Text(
+                        "1 of  Tasks",
+                        style: textTheme.titleMedium,
+                      )
+                    ],
                   )
-                ,
-                25.w,
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      AppStr.mainTitle,
-                      style: textTheme.displayLarge,
-                    ),
-                    3.h,
-                    Text(
-                      "1 of  Tasks",
-                      style: textTheme.titleMedium,
-                    )
-                  ],
-                )
                 ],
               ),
             ),
@@ -77,16 +71,45 @@ class _HomeViewState extends State<HomeView> {
               ),
             ),
 
-
-
-
             //TASKS
-            
+            SizedBox(
+              height: 745,
+              width: double.infinity,
+              child: ListView.builder(
+                  itemCount: 20,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return AnimatedContainer(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      duration: const Duration(microseconds: 600),
+                      decoration: BoxDecoration(
+                          color: AppColors.primaryColor.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(.1),
+                                offset: const Offset(0, 4),
+                                blurRadius: 10)
+                          ]),
+                      child: ListTile(
+                        leading: AnimatedContainer(
+                            duration: const Duration(milliseconds: 600),
+                            decoration: BoxDecoration(
+                              color: AppColors.primaryColor,
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.grey,width: .8),
+
+                            ),
+                            child: const Icon(Icons.check,color: Colors.white,),
+                            ),
+                      ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
     );
   }
-
-
 }
