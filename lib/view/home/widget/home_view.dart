c56d70e4e4a0f93ea814176980d4501bafe9074c
samidev016 <adelim0555@gmail.com>
2024-07-13 20:46:16 +0200

    app bar

--- lib/view/home/widget/home_view.dart
+++ lib/view/home/widget/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todoapplication/extentions/space_exs.dart';
+import 'package:todoapplication/utils/app_colors.dart';
+import 'package:todoapplication/utils/app_str.dart';
 import 'package:todoapplication/view/home/widget/fab.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,6 +14,9 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       backgroundColor: Colors.white,
 
@@ -22,12 +28,60 @@ class _HomeViewState extends State<HomeView> {
         width: double.infinity,
         child: Column(
           children: [
+
+
+            //Costum App BAR
             Container(
-              margin: EdgeInsets.only(top: 60),
+              margin: const EdgeInsets.only(top: 60),
               height: 100,
               width: double.infinity,
-              color: Colors.red,
-            )
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    width: 25,
+                    height: 25,
+                    child: CircularProgressIndicator(
+                      value: 0.3,
+                      backgroundColor: Colors.grey,
+                      valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),
+                    ),
+                  )
+                ,
+                25.w,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      AppStr.mainTitle,
+                      style: textTheme.displayLarge,
+                    ),
+                    3.h,
+                    Text(
+                      "1 of  Tasks",
+                      style: textTheme.titleMedium,
+                    )
+                  ],
+                )
+                ],
+              ),
+            ),
+
+            //Divider
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Divider(
+                thickness: 2,
+                indent: 100,
+              ),
+            ),
+
+
+
+
+            //TASKS
+            
           ],
         ),
       ),
