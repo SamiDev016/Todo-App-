d5f02259358d0adb46ad2f6dc6d848300eaba749
samidev016 <adelim0555@gmail.com>
2024-07-15 21:10:05 +0200

    main Task View Activity

--- lib/view/tasks/task_view.dart
+++ lib/view/tasks/task_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_cupertino_date_picker_fork/flutter_cupertino_date_picker_fork.dart';
 import 'package:todoapplication/extentions/space_exs.dart';
 import 'package:todoapplication/utils/app_str.dart';
 import 'package:todoapplication/view/tasks/components/rep_text_field.dart';
 import 'package:todoapplication/view/tasks/widgets/task_view_app_bar.dart';
 
 class TaskView extends StatefulWidget {
-
   const TaskView({super.key});
 
   @override
@@ -21,7 +22,7 @@ class _TaskViewState extends State<TaskView> {
     var textTheme = Theme.of(context).textTheme;
 
     return GestureDetector(
-      onTap: ()=>FocusManager.instance.primaryFocus!.unfocus(),
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
       child: Scaffold(
         appBar: const TaskViewAppBar(),
         body: SizedBox(
@@ -31,36 +32,74 @@ class _TaskViewState extends State<TaskView> {
             child: Column(
               children: [
                 _buildTopSideTexts(textTheme),
-                SizedBox(
-                  height: 550,
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 30),
-                        child: Text(
-                          AppStr.titleOfTitleTextField,
-                          style: textTheme.headlineMedium,
-                        ),
-                      ),
-
-                      //Task Title
-                      RepTextField(controller: titleTaskController),
-
-                      10.h,
-
-                      RepTextField(controller: descTaskController,isForDesc: true,),
-
-                    ],
-                  ),
-                )
+                
+                _buildMainTaskViewActivity(textTheme, context)
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildMainTaskViewActivity(TextTheme textTheme, BuildContext context) {
+    return SizedBox(
+                height: 550,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30),
+                      child: Text(
+                        AppStr.titleOfTitleTextField,
+                        style: textTheme.headlineMedium,
+                      ),
+                    ),
+
+                    //Task Title
+                    RepTextField(controller: titleTaskController),
+
+                    10.h,
+
+                    RepTextField(
+                      controller: descTaskController,
+                      isForDesc: true,
+                    ),
+
+                    DateTimeWidget(
+                      onTap: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (_) => SizedBox(
+                                  height: 280,
+                                  child: TimePickerWidget(
+                                    //initDateTime: ,
+                                    onChange: (_, __) {},
+                                    dateFormat: "HH:mm",
+                                    onConfirm: (dataTime, _) {},
+                                  ),
+                                ));
+                      },
+                      title: AppStr.timeString,
+                    ),
+
+                    DateTimeWidget(
+                      onTap: () {
+                        DatePicker.showDatePicker(
+                          context,
+                          maxDateTime: DateTime(2030,5,5),
+                          minDateTime: DateTime.now(),
+                          //initialDateTime: ,
+                          onConfirm: (context,_){},
+
+                        );
+                      },
+                      title: AppStr.dateString,
+                    ),
+                  ],
+                ),
+              );
   }
 
   Widget _buildTopSideTexts(TextTheme textTheme) {
@@ -94,6 +133,57 @@ class _TaskViewState extends State<TaskView> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class DateTimeWidget extends StatelessWidget {
+  const DateTimeWidget({
+    super.key,
+    required this.onTap,
+    required this.title,
+  });
+
+  final VoidCallback onTap;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(20),
+        width: double.infinity,
+        height: 55,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey.shade300),
+            borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(title, style: textTheme.headlineSmall),
+            ),
+            Container(
+              margin: const EdgeInsets.only(right: 10),
+              width: 80,
+              height: 35,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.grey.shade100),
+              child: Center(
+                child: Text(
+                  title,
+                  style: textTheme.titleSmall,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
